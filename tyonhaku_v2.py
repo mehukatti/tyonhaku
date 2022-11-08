5286ce6b83654793e26de9093798a943a2b34aae
mehukatti <janniinakarinsalo@gmail.com>
2022-11-08 07:03:35 +0200

    Update tyonhaku_v2.py

--- tyonhaku_v2.py
+++ tyonhaku_v2.py
@@ -8,21 +8,27 @@ import time
 import pandas as pd
 import os
 
-class job_search_app(Frame):
-    start_time = None
+#Fingelskaa. Muutan kaiken suomeksi myöhemmin.
+class tyonhaku_sovellus(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master = master
-        self.create_widgets()
-    def create_widgets(self):
-        canvas = Canvas(self) #Tarvitaanko tätä?
+        self.luo_widgetit()
+    def luo_widgetit(self):
+        canvas = Canvas(self)
 
         #Ilmoitus tiedostojen sijainnista ja tuloksista
         self.tuloskohta = tk.Label(self.master, text=' ')
         self.tuloskohta.grid(row=5,columnspan=4, pady=5)
 
         #Tarkistus-nappi haun tarkistuksen jälkeen
-        self.tarkistus_nappi = Button(self.master, text='Tarkista', command=osuvuudet)#state=DISABLED, 
+        """
+        Haluan eroon tästä napista. Tämä tarkistaa, onko tulosexcelissä 1 vai 0 kullakin rivillä.
+        Jos se on 0, lisätään kiellettyjen kategorioiden listalle.
+        Tämän valinta ei ole hakukohtainen, vaan koskee jokaista hakua.
+        Tällä hetkellä kategorioita ei voi valita kuin excelin tai suoraan jsonin kautta.
+        """
+        self.tarkistus_nappi = Button(self.master, text='Tarkista', command=osuvuudet) 
         self.tarkistus_nappi.grid(row=6,column=3, columnspan=2, padx=5, pady=5)
 
         #Hakusanat-tekstiruutu
@@ -50,18 +56,54 @@ class job_search_app(Frame):
         self.tyokkari = tk.Checkbutton(self.master, text="Työvoimatoimisto", variable=self.tyokkari_luku, onvalue=1, offvalue=0)
         
         self.duunitori.grid(row=6,column=0, padx=5)
-        self.tekstista.grid(row=8,column=0, padx=5)
+        self.tekstista.grid(row=7,column=0, padx=5)
         self.tyokkari.grid(row=6,column=1, padx=5)
+
+        #duunitorin kategorioiden valinta
+        """
+        duunitorin kategorioiden valinta parantaa hakutuloksia, kun hakee esim. bioalalta.
+        Tällä hetkellä niiden valinta tehdään lisäämällä tulosexceliin 1 tai 0.
+        Ei ole kovin näppärää ja haluaisin lisätä myös sen valinnan.
+        Kategorioita on kuitenkin useita ja haluaisin löytää jonkin järkevän monivalinta-vaihtoehdon.
+        """
+        #tk.Label(self.master, text='Duunitorin kategoriat', font=('helvetica', 10)).grid(row=8, sticky='w', column=0, padx=5)
+        #kaikissa on oletuksena 1 eli oletuksena valittu
+        self.duunitorin_kategoriat = {
+            "asennus, huolto ja puhtaanapito (ala)": 1,
+            "asiakaspalvelu (ala)": 1,
+            "asiantuntijatyöt ja konsultointi (ala)": 1,
+            "hallinto ja yleiset toimistotyöt (ala)": 1,
+            "henkilöstöala (ala)": 1,
+            "hyvinvointi ja henkilöpalvelut (ala)": 1,
+            "johtotehtävät (ala)": 1,
+            "julkinen sektori ja järjestöt (ala)": 1,
+            "kiinteistöala (ala)": 1,
+            "kuljetus, logistiikka ja liikenne (ala)": 1,
+            "kulttuuri-, viihde- ja taidealat (ala)": 1,
+            "lakiala (ala)": 1,
+            "markkinointi (ala)": 1,
+            "myynti ja kaupan ala (ala)": 1,
+            "opetusala (ala)": 1,
+            "opiskelijoiden työpaikat (ala)": 1,
+            "rakennusala (ala)": 1,
+            "ravintola- ja matkailuala (ala)": 1,
+            "sosiaali- ja hoiva-ala (ala)": 1,
+            "taloushallinto ja pankkiala (ala)": 1,
+            "teollisuus ja teknologia (ala)": 1,
+            "terveydenhuoltoala (ala)": 1,
+            "tieto- ja tietoliikennetekniikka (ala)": 1,
+            "turvallisuusala (ala)": 1,
+        }
         
         #Hakujen rajoitus ilmoitusajankohdan mukaan
         self.tyokkari_aika = tk.Label(self.master, text='Julkaistu')
         self.tyokkari_aika.grid(row=8, column=1, sticky='w', pady=5, padx=5)
-        self.option = tk.IntVar()#option on julkaisuajankohdan valinta
+        self.ilmoitusajan_valinta = tk.IntVar()
         
-        self.kaikki = Radiobutton(self.master, text="kaikki", value=0, variable=self.option)
-        self.vuorokausi = Radiobutton(self.master, text="vuorokausi", value=1, variable=self.option)
-        self.kolme_paivaa = Radiobutton(self.master, text="3 päivää", value=2, variable=self.option)
-        self.viikko = Radiobutton(self.master, text="viikko", value=3, variable=self.option)
+        self.kaikki = Radiobutton(self.master, text="kaikki", value=0, variable=self.ilmoitusajan_valinta)
+        self.vuorokausi = Radiobutton(self.master, text="vuorokausi", value=1, variable=self.ilmoitusajan_valinta)
+        self.kolme_paivaa = Radiobutton(self.master, text="3 päivää", value=2, variable=self.ilmoitusajan_valinta)
+        self.viikko = Radiobutton(self.master, text="viikko", value=3, variable=self.ilmoitusajan_valinta)
         
         self.kaikki.grid(row=9, column=1, sticky='w', pady=5, padx=5)
         self.vuorokausi.grid(row=10, column=1, sticky='w', pady=5, padx=5)
@@ -76,39 +118,41 @@ class job_search_app(Frame):
         self.nimella_kentta.grid(row=14, sticky='w', column=0, padx=5)
         
         #Tallennettu haku
-        #self.tallennettu_nappi = Button(self.master, text='Tallennettu haku', command=start.load_saved_input)
         self.tallennettu_nappi = Button(self.master, text='Tallennettu haku', command=self.select_saved_search)
         self.tallennettu_nappi.grid(row=16,column=1, columnspan=2, padx=5, pady=5)
         
         #Hae-nappi
-        #self.haku_nappi = Button(self.master, text='Hae', command=start.take_input)
         self.haku_nappi = Button(self.master, text='Hae', command=self.take_input)
         self.haku_nappi.grid(row=15,column=0, columnspan=2, padx=5)
         #Lopetus-nappi
         self.lopeta_nappi = Button(self.master, text='Lopeta', command=self.master.destroy)
         self.lopeta_nappi.grid(row=15,column=2, padx=5)
 
+        #jos haluat muuttaa tulosexcelin tai tietojen tallennustiedoston nimeä, muuta tästä.
+        self.tallennettujen_hakujen_polut = "names_of_saved_param.json" #tänne tallennetaan tallennettujen hakujen nimet 
+        self.tulosexcelin_polku = 'Hakutulokset.xlsx' #tänne tallennetaan suoritetun haun tulokset (titteli, paikka, linkki, ilmoituspäivä)
 
-    def create_paths(self):
-        self.path_to_saved_named_searches = "names_of_saved_param.json"
-        self.path_to_results_xlsx = 'Hakutulokset.xlsx'
+        #jotta voi laskea, kauanko haku kestää
+        self.aloitus_aika = None
 
-    def save_input(self, nimi):
-        saved_parameters = {
-            "search_words": self.hakusana_kentta.get().split(", "),
+    def tallenna_GUI_input(self, nimi):
+        tallennetut_parametrit = {
+            #koska laitoin vain yhden kentän per hakusana ja sijainti, pitää useat hakusanat ja sijainnit erottaa jollakin merkillä.
+            "search_words": self.hakusana_kentta.get().split(", "), 
             "locations": self.sijainti_kentta.get().split(", "),
             "duunitori": self.duunitori_luku.get(),
             "tetoimisto": self.tyokkari_luku.get(),
-            "published":self.option.get(),
+            "published":self.ilmoitusajan_valinta.get(),
             "duunitori_from_text_also": self.duunitori_tekstista.get(),
             "not_with_these_words": self.ei_nailla_titteleilla.get().split(", ")
         }
-        access_files.write_data_to_json_with(saved_parameters, nimi + '.json')
+        #tallennetaan hakutekijät omalla nimellä yhteen tiedostoon
+        access_files.write_data_to_json_with(tallennetut_parametrit, nimi + '.json')
 
     
     def take_input(self): #kerää käyttäjän antamat inputit
-        self.clear_other_data()
-        start_time = time.time()
+        self.tyhjenna_tulosteksti_GUIssa()
+        self.aloitus_aika = time.time() #jotta saadaan laskettua haun kesto
         if (self.duunitori_luku.get() == 0) & (self.tyokkari_luku.get() == 0):
             self.tuloskohta.config(text="Valitse joku hakusivu.") #jos mitään työnhakusivua ei valittu, käskee valitsemaan
         else:
@@ -118,20 +162,20 @@ class job_search_app(Frame):
                     self.tuloskohta.config(text="Lisää tallennetulle haulle nimi.")
                 else:
                     #check if the given name already exists.
-                    names_of_saved_parameters = access_files.open_json(self.path_to_saved_named_searches)
-                    if tallennetun_nimi in names_of_saved_parameters:
-                        self.tuloskohta.config(text="Name already exists. Give a new one.")
+                    tallennettujen_hakujen_nimet = access_files.open_json(self.tallennettujen_hakujen_polut)
+                    if tallennetun_nimi in tallennettujen_hakujen_nimet:
+                        self.tuloskohta.config(text="Tällä nimellä on jo tallennettu hakuparametrit. Anna toinen nimi.")
                     else:
-                        names_of_saved_parameters.append(tallennetun_nimi)
-                        access_files.write_data_to_json_with(names_of_saved_parameters, self.path_to_saved_named_searches)
-                        self.save_input(tallennetun_nimi)
-        print("hakusivun_valinta. " + str(time.time()-start_time))
-        self.en_disable('disable')
-        self.hakusivun_valinta(start_time)
+                        tallennettujen_hakujen_nimet.append(tallennetun_nimi)
+                        access_files.write_data_to_json_with(tallennettujen_hakujen_nimet, self.tallennettujen_hakujen_polut)
+                        self.tallenna_GUI_input(tallennetun_nimi)
+        print("hakusivun_valinta. " + str(time.time()-self.aloitus_aika))
+        self.en_disable_widgets('disable')
+        self.hakusivun_valinta(self.aloitus_aika)
 
     def select_saved_search(self):
         #avataan tiedosto ja luodaan sen perusteella dropdown menu
-        saved_searches_options = access_files.open_json(self.path_to_saved_named_searches)
+        saved_searches_options = access_files.open_json(self.tallennettujen_hakujen_polut)
         if len(saved_searches_options) == 0:
             self.tuloskohta.config(text="Ei tallennettuja hakusanoja. Anna hakusanat ja paina Hae-nappia.")
         else:
@@ -148,46 +192,46 @@ class job_search_app(Frame):
 
     def load_saved_input(self):
         tallennetut_tiedot = access_files.open_json(self.named_searches.get() + ".json")
-        self.clear_data_for_saved_input()
-        self.clear_other_data()
-        start_time = time.time()
+        self.tyhjenna_GUI_input()
+        self.tyhjenna_tulosteksti_GUIssa()
+        self.aloitus_aika = time.time()
 
-        #self.nimella_kentta.insert(0, self.named_searches.get())
         self.hakusana_kentta.insert(0, access_files.list_to_string(tallennetut_tiedot["search_words"], ", "))
         self.sijainti_kentta.insert(0, access_files.list_to_string(tallennetut_tiedot["locations"], ", "))
         self.duunitori_luku.set(int(tallennetut_tiedot["duunitori"]))
         self.tyokkari_luku.set(int(tallennetut_tiedot["tetoimisto"]))
-        self.option.set(int(tallennetut_tiedot["published"]))
+        self.ilmoitusajan_valinta.set(int(tallennetut_tiedot["published"]))
         self.duunitori_tekstista.set(tallennetut_tiedot["duunitori_from_text_also"])
         self.ei_nailla_titteleilla.insert(0, access_files.list_to_string(tallennetut_tiedot["not_with_these_words"], ", "))
         self.saved_search_info = tk.Label(self.master, text='Voit muokata ennen kuin painat Jatko-nappia ja tiedot tallentuvat automaattisesti.')
         self.saved_search_info.grid(row=20,columnspan=4, pady=5)
 
         #lopuksi luo jatkonapin ja poisto-napin
-        self.continue_saved_search_button = Button(self.master, text='Jatka tallennettua hakua', command=self.continue_search)
-        self.continue_saved_search_button.grid(row=18,column=1, columnspan=2, padx=5, pady=5)
+        self.continue_saved_search_button = Button(self.master, text='Jatka tallennettua hakua', command=self.hae_tallennetuilla_parametreilla)
+        self.continue_saved_search_button.grid(row=18,column=2, columnspan=2, padx=5, pady=5)
         self.remove_saved_search_button = Button(self.master, text='Poista tallennettu haku', command=self.remove_selected_search_parameters)
         self.remove_saved_search_button.grid(row=18,column=0, columnspan=2, padx=5, pady=5)
 
     def remove_selected_search_parameters(self):
+        #poistaa ne tallennetut hakuparametrit, jotka on valittuna.
         remove_this = self.named_searches.get()
         #tahan viela vahvistuslaatikko voisi olla ok, mutta en nyt jaksa tehda.
-        names_of_saved_params = access_files.open_json(self.path_to_saved_named_searches)
+        names_of_saved_params = access_files.open_json(self.tallennettujen_hakujen_polut)
         if remove_this in names_of_saved_params:
             names_of_saved_params.remove(remove_this)
-            access_files.write_data_to_json_with(names_of_saved_params, self.path_to_saved_named_searches)
+            access_files.write_data_to_json_with(names_of_saved_params, self.tallennettujen_hakujen_polut)
             os.remove(remove_this + ".json")
             self.saved_search_info.config(text="poistettu tallennetut hakutiedot nimellä: " + remove_this)
             self.select_saved_search #this does not update optionmenu
         else:
             self.saved_search_info.config(text="virhe yrittäessä poistaa hakutietoja nimellä: " + remove_this)
 
-    def continue_search(self):
-        self.en_disable("disabled")
-        self.save_input(self.named_searches.get()) #tallenna sillä nimellä, mikä valittiin
+    def hae_tallennetuilla_parametreilla(self):
+        self.en_disable_widgets("disabled") #estää GUI:n käytön haun aikana
+        self.tallenna_GUI_input(self.named_searches.get()) #tallenna sillä nimellä, mikä valittiin
         self.hakusivun_valinta(time.time())
 
-    def hakusivun_valinta(self,start_time):
+    def hakusivun_valinta(self, aloitus_aika):
         print("hakusivun valinta")
         sana, paikka, ei_tittelit = access_files.nolla_tyhjaksi(self.hakusana_kentta.get()), access_files.nolla_tyhjaksi(self.sijainti_kentta.get()), access_files.nolla_tyhjaksi(self.ei_nailla_titteleilla.get())
         sanalista = sana.split(", ")
@@ -195,10 +239,10 @@ class job_search_app(Frame):
         ei_tittelit_lista = ei_tittelit.split(", ")
         tulostaulukko = pd.DataFrame()
         if (self.duunitori_luku.get() == 1):
-            tulostaulukko = suorita_haku_duunitori(sanalista, paikkalista, ei_tittelit_lista, self.duunitori_tekstista.get(), self.option.get()) #option on julkaisupäivän valinta
+            tulostaulukko = suorita_haku_duunitori(sanalista, paikkalista, ei_tittelit_lista, self.duunitori_tekstista.get(), self.ilmoitusajan_valinta.get()) #option on julkaisupäivän valinta
         if (self.tyokkari_luku.get() == 1):
             #lisäsin kielletyt tittelit työkkärin hakuun mukaan
-            tulostaulukko = tulostaulukko.append(suorita_haku_tyovoimatoimisto(sana, paikka, str(self.option.get()), ei_tittelit_lista), ignore_index = True) #Tässä haluan poistaa samat rivit vaan toisesta taulukosta
+            tulostaulukko = tulostaulukko.append(suorita_haku_tyovoimatoimisto(sana, paikka, str(self.ilmoitusajan_valinta.get()), ei_tittelit_lista), ignore_index = True) #Tässä haluan poistaa samat rivit vaan toisesta taulukosta
             tulostaulukko = tulostaulukko.drop_duplicates(subset=['Titteli', 'Työnantaja', 'Sijainti']) #tämä ei ihan toimi
         if (tulostaulukko.empty):
             self.tuloskohta.config(text="Ei hakutuloksia hakusanoilla.")
@@ -207,30 +251,37 @@ class job_search_app(Frame):
             tulostaulukko.to_excel('Hakutulokset.xlsx', index = False)
             self.tuloskohta.config(text="Valmis. Tarkista tulokset ja merkitse huonot ('0') ja hyvät ('1'). Paina 'Tarkista'.")
             self.change_tarkistus("normal")
-            print(time.time() - start_time)
-        self.en_disable("normal")
+            print(time.time() - self.aloitus_aika)
+        self.en_disable_widgets("normal") #sallii GUI:n käytön haun jälkeen
     
     #tkinterin widgetien värit ja klikkauksien estot
-    def en_disable(self,string):
+    def en_disable_widgets(self,string): #string voi olla joko "disabled" tai "normal". Jos on "disabled", ei voi klikkailla tai kirjoittaa ja jos on "normal", voi muokata.
         widgets = self.hakusana_kentta, self.sijainti_kentta, self.tekstista, self.duunitori, self.tyokkari, self.kaikki, self.vuorokausi, self.kolme_paivaa, self.viikko, self.tallenna_haku, self.tallennettu_nappi, self.haku_nappi, self.ei_nailla_titteleilla
         for widget in widgets:
             widget.config(state=string)
         self.update()
         return;
-    def clear_data_for_saved_input(self):
+
+    def tyhjenna_GUI_input(self):
+        #Kun syötetään tallennettuja tietoja, pitää varmistaa, että käyttäjä ei ollut jo antanut joitain tietoja.
         self.hakusana_kentta.delete(0, END)
         self.sijainti_kentta.delete(0, END)
         self.ei_nailla_titteleilla.delete(0, END)
         return;
-    def clear_other_data(self):
+
+    def tyhjenna_tulosteksti_GUIssa(self):
+        #tyhjentää hakukenttien alle tulevan kohdan, jonne tulee viestejä ja ohjeita
         self.tuloskohta.config(text=" ")
         return;
+
     def change_tarkistus(self,string):
+        #Kun syötetään tallennettuja tietoja, pitää varmistaa, että käyttäjä ei ollut jo antanut joitain tietoja.
+        #ei ole mitään syytä laittaa tämä nappi erikseen disabloitavaksi ja hyväksyttäväksi. Toisaalta haluan poistaa tämän napin, mutta se edellyttää vähän enemmän työtä.
         self.tarkistus_nappi.config(state=string)
 
 #luo ikkunan
-window = tk.Tk()
-window.title('Super työnhaku')
-window.geometry('700x900')
-window = job_search_app(window)
-window.mainloop()
+ikkuna = tk.Tk()
+ikkuna.title('Super työnhaku')
+ikkuna.geometry('700x900')
+ikkuna = tyonhaku_sovellus(ikkuna)
+ikkuna.mainloop()
